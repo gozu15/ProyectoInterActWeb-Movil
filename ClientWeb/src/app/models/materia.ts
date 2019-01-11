eac1672f67f1377617e1b7cd3c02974ca31a9c31
wilson-pc <wilverhidalgo@outlook.com>
2019-01-11 10:44:20 -0400

    registrar docente

--- ClientWeb/src/app/models/materia.ts
+++ ClientWeb/src/app/models/materia.ts
@@ -2,7 +2,7 @@ export class Materia {
     public id: number;
     public nombre: string;
     public descripcion:string;
-    public creacion:{fecha:Date,usuario:string};
-    public modificacion:{fecha,usuarios:string};
+    public creacion:{fecha:any,usuario:string};
+    public modificacion:{fecha:any,usuario:string};
     public eliminado:{estado:boolean,razon:string};
 }
--- ClientWeb/src/app/registro-docente-page/registro-docente-page.component.ts
+++ ClientWeb/src/app/registro-docente-page/registro-docente-page.component.ts
@@ -1,15 +1,78 @@
+import { UsuarioService } from './../service/usuario.service';
+import { DocenteService } from './../service/docente.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Docente } from '../models/docente';
+import { getBase64, resizeBase64 } from 'base64js-es6';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-registro-docente-page',
   templateUrl: './registro-docente-page.component.html',
   styleUrls: ['./registro-docente-page.component.css']
 })
 export class RegistroDocentePageComponent implements OnInit {
-
-  constructor() { }
+docente:Docente;
+image:any;
+isRequired: boolean = false;
+isError: boolean = false;
+isExito:boolean=false;
+recordar: boolean = false;
+  constructor(private datePipe: DatePipe,private usuarioserv:UsuarioService) {
+    this.docente=new Docente;
+    
+   }
 
   ngOnInit() {
   }
 
+  RegistrarDocente(nacimiento){
+    this.isError = false;
+    this.isRequired = false;
+    this.isExito=false;
+  var date = new Date();
+  this.docente.fechadenacimiento=nacimiento;
+  this.docente.creacion={fecha:this.datePipe.transform(date,"yyyy-MM-dd HH:mm:ss"),usuario:"5c34b3a83619a9178c5902f1"};
+  this.docente.modificacion={fecha:this.datePipe.transform(date,"yyyy-MM-dd HH:mm:ss"),usuario:"5c34b3a83619a9178c5902f1"};
+
+  
+ if(this.docente.nombre){
+  
+  this.usuarioserv.RegistrarDocente(this.docente).subscribe((docente: any) => {
+  
+    if (docente) {
+     
+    this.isExito=true;
+    console.log(docente);
+    this.docente=new Docente;
+    } else {
+      alert('error desconocido');
+    }
+  }, (error: any) => {
+    this.isError = true;
+  });
+ }
+ else{
+this.isRequired=true;
+ }
+  }
+
+  changeListener($event) : void {
+    this.readThis($event.target);
+  }
+  
+  readThis(inputValue: any): void {
+    var file:File = inputValue.files[0];
+    console.log(inputValue.files[0]);
+    this.docente.perfil={tipo:inputValue.files[0].type,foto:"",miniatura:""};
+    var myReader:FileReader = new FileReader();
+  
+    myReader.onloadend = (e) => {
+      this.docente.perfil.foto=myReader.result.toString();
+      resizeBase64(myReader.result, 200, 100).then((result) => {
+       this.docente.perfil.miniatura=result;
+
+      });
+    
+    }
+    myReader.readAsDataURL(file);
+  }
 }
