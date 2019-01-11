4a01f7b06d3adc4c4fcbf49bb9bd0dbabd757e48
Angel <acherfun99@gmail.com>
2019-01-11 10:45:21 -0400

    Agregando modal al registro de docentes

--- ClientWeb/src/app/registro-docente-page/registro-docente-page.component.ts
+++ ClientWeb/src/app/registro-docente-page/registro-docente-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-registro-docente-page',
@@ -6,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro-docente-page.component.css']
 })
 export class RegistroDocentePageComponent implements OnInit {
+  closeResult: string;
+  constructor(private modalService: NgbModal) { }
 
-  constructor() { }
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      console.log("entro al boton dismiss");
+      //TODO poner codigo de obtencion de datos del formulario y envio al backend
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      console.log("entro modaldismiss");
+      return 'by clicking on a backdrop';
+    } else {
+      console.log("entro a otro lado")
+      return  `with: ${reason}`;
+    }
+  }
   ngOnInit() {
   }
 
